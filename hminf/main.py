6bc5ce4f34d16d9b0ebc2ec025eb8bf4ac981262
moe <moe.alibr@gmail.com>
2025-12-12 23:07:03 +0100

    switched to raylib

--- hminf/main.py
+++ hminf/main.py
@@ -1,88 +1,59 @@
 from __future__ import annotations
-
-import pygame
 from contextlib import contextmanager
-from typing import Final, NamedTuple, final
+from typing import Final, NamedTuple, final,Generator
 
+import pyray
 
-@final
-class PyGameContext(NamedTuple):
-    screen: pygame.Surface
-    clock: pygame.time.Clock
+class Context(NamedTuple):
+    screen_width: int
+    screen_height: int
 
-    def flip(self):
-        pygame.display.flip()
+    def begin(self) -> None:
+        pyray.begin_drawing()
 
-    def get_events(self):
-        return pygame.event.get()
+    def end(self) -> None:
+        pyray.end_drawing()
 
-    def get_pressed_keys(self):
-        return pygame.key.get_pressed()
+    def get_frame_time(self) -> float:
+        return pyray.get_frame_time()
 
-    def tick(self, fps: int):
-        return self.clock.tick(fps)
+    def should_close(self) -> bool:
+        return pyray.window_should_close()
+
+    def is_key_down(self, key: int) -> bool:
+        return pyray.is_key_down(key)
+
+    def is_key_pressed(self, key: int) -> bool:
+        return pyray.is_key_pressed(key)
+
 
 @contextmanager
-def create_game(width: int = 800, height: int = 800, title: str = "HMINF"):
+def create_context(
+    width: int = 1280,
+    height: int = 720,
+    title: str = "HMINF",
+    target_fps: int = 60,
+) -> Generator[Context, None, None]:
+
+    pyray.init_window(width, height, title)
+    pyray.set_target_fps(target_fps)
 
     try:
-        pygame.init()
-        pygame.display.set_caption(title)
-
-        screen: Final = pygame.display.set_mode((width, height))
-        clock: Final = pygame.time.Clock()
-
-        ctx = PyGameContext(
-            screen=screen,
-            clock=clock,
+        yield Context(
+            screen_width=width,
+            screen_height=height,
         )
-        yield ctx
-
     finally:
-        pygame.quit()
-
+        pyray.close_window()
 
 
 def main():
-    crcl_x = 100
-    crcl_y = 200
-    crcl_radius = 30
-    box_width = 700
-    box_height = 700
-    box_left_border = 50
-    box_upper_border = 50
-    crcl_speed_x = 2
-    crcl_speed_y = 2
-    with create_game() as context:
-        running = True
-        while running:
-            for event in context.get_events():
-                if event.type == pygame.QUIT:
-                    running = False
-                    print("Spiel durch Benutzer beendet")
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_m:
-                        print("Taste gedrückt")
-
-            context.screen.fill("black")
-            pygame.draw.rect(context.screen, "red", [50, 50, box_height, box_width], 3)
-            pygame.draw.circle(context.screen, "white", [crcl_x, crcl_y], crcl_radius)
-            if crcl_x >= box_width:
-                crcl_speed_x = -crcl_speed_x
-            if crcl_x-crcl_radius <= box_left_border:
-                crcl_speed_x = -crcl_speed_x
-
-            if crcl_y >= box_height:
-                crcl_speed_y = -crcl_speed_y
-            if crcl_y-crcl_radius <= box_upper_border:
-                crcl_speed_y = -crcl_speed_y
-            crcl_x += crcl_speed_x
-            crcl_y += crcl_speed_y
-            #crcl_radius += 0.05
-
-
-            context.flip()
-            context.tick(60)
+    with create_context() as context:
+        while not context.should_close():
+            context.begin()
+            clear_background(WHITE)
+            draw_text("HMINF", int(context.screen_width / 2), 40, 120, GREEN)
+            context.end()
 
 if __name__ == '__main__':
     main()
