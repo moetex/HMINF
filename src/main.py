ca6753d5f02b78510b4e8b4ea6cdb16bf5ff4869
Mathis <mgronewold@stud.hs-bremen.de>
2026-03-02 12:11:07 +0100

    Main verändert, damit STL Datei auch in Exe funktioniert

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import sys
 import traceback
+from pathlib import Path
+import os, sys
 
 from src.controller.programController import Simulation
 
@@ -8,16 +10,48 @@ from src.controller.programController import Simulation
 # ----------------------------------------
 # Main Entry Point
 # ----------------------------------------
+def resource_path(filename: str) -> str:
+    if getattr(sys, "frozen", False):
+        exe_dir = Path(sys.executable).resolve().parent  # dist\main
+
+        candidates = [
+            exe_dir / "res" / filename,
+            exe_dir / "_internal" / "res" / filename,
+            exe_dir / "_internal" / filename,   # fallback falls target anders war
+        ]
+
+        # last resort: suchen
+        hits = list(exe_dir.rglob(filename))
+        if hits:
+            return str(hits[0])
+
+        raise FileNotFoundError(f"{filename} not found. Tried: {candidates}")
+
+    # normaler Run aus src/
+    project_root = Path(__file__).resolve().parents[1]
+    return str(project_root / "res" / filename)
 
 def main():
     """Haupteinstiegspunkt"""
-    stl_path = "../res/labubu_keychain_voxel_mc_16k.stl"
+    stl_path = resource_path("labubu.stl")
+    with open(stl_path, "rb") as f:
+        f.read(16)
+    print("DEBUG STL open OK")
 
     try:
+        print("DEBUG sys.executable:", sys.executable)
+        print("DEBUG cwd:", os.getcwd())
+        print("DEBUG stl_path repr:", repr(stl_path))
+
+        p = Path(stl_path)
+        print("DEBUG exists:", p.exists(), "is_file:", p.is_file())
+        print("DEBUG abs:", str(p.resolve()))
+        print("DEBUG parent list:", [x.name for x in p.parent.glob("*")])
         sim = Simulation(stl_path)
         sim.run()
-    except FileNotFoundError:
-        print(f"STL-Datei nicht gefunden: {stl_path} ")
+    except FileNotFoundError as e:
+        print("FileNotFoundError:", e)
+        traceback.print_exc()
         sys.exit(1)
     except Exception as e:
         print(f"Ein unerwarteter Fehler ist aufgetreten: {e} ")
@@ -29,4 +63,3 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         print(e)
-
